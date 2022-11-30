dfd28e722bb92c3dc34b7d3db99a324ba1845655
Konektive <85581900+Konektive@users.noreply.github.com>
2022-11-30 22:43:41 +0100

    Add files via upload
    Added some simple css to posts

--- Blog/src/pages/Posts.jsx
+++ Blog/src/pages/Posts.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-// import classes from './Posts.modules.css'
-import { NavLink, useParams } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import image from '../assets/landscape-1.jpg'
+import classes from './Posts.module.css'
 
 const Posts = () => {
   const [posts, setPosts] = useState([]);
@@ -15,13 +16,28 @@ const Posts = () => {
     });
   }, []);
 
+
   return posts.map((post) => {
+    const item = new Date(post.date)
+    const year = item.getFullYear()
+    const month = item.getMonth()
+    const day = item.getDay()
     return (
-      <div className="wrapper" key={post._id}>
-        <h1>{post.title}</h1>
-        <div>{post.text}</div>
-        <span><NavLink to={`/${post._id}`}>Read more...</NavLink></span>
-      </div>
+      <NavLink key={post._id} to={`/${post._id}`}>
+      <div className={classes.wrapper} >
+        <div className={classes.img}>
+          <img src={image} alt="Landscape" />
+        </div>
+            <div className={classes.info}>
+            <h1>{post.title}</h1>
+            <h2>{`${year}:${month}:${day}`}</h2>
+          <span>{post.text}</span>
+            <br />
+            {/* Decide if u want to have a read more span or full post to be a link to it  */}
+            {/* <span><NavLink to={`/${post._id}`}>Read more...</NavLink></span>   */}
+        </div>
+        </div>
+        </NavLink>
     );
   });
 };
