a3d1bf4fca8d9710ad16792e0ae91a8a6d4585b3
Krystian <krystiansporysz@gmail.com>
2023-03-09 14:25:03 +0100

    Added login form

---
+++ Blog/src/components/forms/Login.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const LoginForm = () => {
+  return (
+    <div><img src="" alt="" />
+    <form>
+  <label>
+    E-mail:
+    <input type="email" name="email" />
+  </label>
+  <label>
+    Password:
+    <input type="password" name="password" />
+  </label>
+<button type='submit'>Login</button>
+  <input type="submit" value="Submit" />
+</form>
+</div>
+  )
+}
+
+export default LoginForm
